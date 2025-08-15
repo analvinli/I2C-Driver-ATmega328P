b4ba6f90396d60ec5fa35f70efa5dfc1893a201d
analvinli <analvinli@gmail.com>
2025-08-15 16:01:46 -0400

    stuff

--- src/twi.c
+++ src/twi.c
@@ -2,4 +2,16 @@
 #include <avr/io.h>
 #include <util/twi.h>
 
+typedef struct{
+    volatile uint8_t busy;
+    volatile twi_message_t *msg;
+    volatile size_t msg_count;
+    volatile twi_status_t state;
+}twi_variables_t;
 
+static volatile twi_variables_t twi_vars;
+static volatile twi_message_t current_msg;
+
+static int calc_bitrate(const uint8_t scl_frequency){
+    return ((F_CPU/scl_frequency)-16)/2; //assume TWPS or prescaler = 1
+}
--- src/twi.h
+++ src/twi.h
@@ -8,6 +8,20 @@
 #define WRITE 0
 #define READ 1
 
+typedef enum {
+    TWI_SUCCESS,
+    TWI_FAILURE,
+    TWI_NORMAL,
+} twi_status_t;
 
+typedef struct {
+    uint8_t address;
+    uint8_t *buffer;
+    uint8_t size;
+} twi_message_t;
+
+
+twi_status_t twi_init(const uint8_t scl_frequency);
+static int calc_bitrate(const uint8_t scl_frequency);
 
 #endif
