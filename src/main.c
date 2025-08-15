ccb739f583d517d4ef0e309adea1ef1a88eb6801
analvinli <analvinli@gmail.com>
2025-08-15 17:40:49 -0400

    main.c test code

--- src/main.c
+++ src/main.c
@@ -4,7 +4,32 @@
 #include <stdbool.h>
 
 int main(){
+    DDRC &= ~(1<<DDC4);//set 4 and 5 as outputs
+    DDRC &= ~(1<<DDC5);
+    PORTC |= (1<<PC4);//enable pulldown resistors
+    PORTC |= (1<<PC5);
+    sei();
+
+    twi_status_t status; 
+    status = twi_init(100000);
+    while (status!=TWI_SUCCESS);
+
+    twi_message_t messages[2] = {
+        {
+            .address = (0x68<<1)|WRITE,
+            .buffer = (uint8_t[1]){0x75},
+            .size = 1,
+        },
+        {
+            .address = (0x68<<1)|READ,
+            .buffer = (uint8_t[1]){},
+            .size = 1,
+        },
+    };
+    twi_enque(messages,2);
+    while(twi_status!=TWI_SUCCESS);
     
+    //messages[1].buffer[0] = 0x68
 
     while(1){
         loop();
